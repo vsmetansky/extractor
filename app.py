82c08bab1ce9890d33632348dd3fb9dd54349bfa
vladsmetansky <vladsmetansky@gmail.com>
2020-02-25 14:26:53 +0200

    cli created

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import argparse
+if __name__ == '__main__':
+    import argparse
 
-parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('website', dest='website')
-parser.add_argument('--page-num', dest='page_num', type=int, default=1)
+    parser.add_argument('website', dest='website')
+    parser.add_argument('-n', '--page-num', dest='page_num', type=int, default=1)
 
-args = parser.parse_args()
+    args = parser.parse_args()
